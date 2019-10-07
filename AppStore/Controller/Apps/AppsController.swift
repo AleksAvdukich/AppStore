c2e8b71917aad098ada1882fa72b672765bcf7d1
Sanel Avdukich <avdukichsanel@mail.ru>
2019-10-07 23:22:39 +0300

    Nested UICollectionViews

---
+++ AppStore/Controller/Apps/AppsController.swift
@@ -0,0 +1,35 @@
+//
+//  AppsController.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 07/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "id"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+        
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) 
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+}
---
+++ AppStore/Controller/BaseListController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseListController.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 07/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class BaseListController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
