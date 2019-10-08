717acb22b9bc28b87c065ee20a72357433f092f4
Sanel Avdukich <avdukichsanel@mail.ru>
2019-10-08 18:47:25 +0300

    Nested Header Collection View

---
+++ AppStore/Controller/Apps/AppsHeaderHorizontalController.swift
@@ -0,0 +1,46 @@
+//
+//  AppsHeaderHorizontalController.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 08/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class AppsHeaderHorizontalController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(AppsHeaderCell.self, forCellWithReuseIdentifier: cellId)
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return .init(width: view.frame.width - 48, height: view.frame.height)
+    }
+    
+    //отступы для всей секции
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 16, bottom: 0, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+}
---
+++ AppStore/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,26 @@
+//
+//  AppsPageHeader.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 08/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(appHeaderHorizontalController.view)
+        appHeaderHorizontalController.view.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
