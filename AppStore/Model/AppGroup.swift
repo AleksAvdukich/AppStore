8a8aaf4a96e24bde6f9d4e923b3310220b70a400
Sanel Avdukich <avdukichsanel@mail.ru>
2019-10-11 03:16:34 +0300

    Fetch App Group JSON Data

---
+++ AppStore/Model/AppGroup.swift
@@ -0,0 +1,22 @@
+//
+//  AppGroup.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 10/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import Foundation
+
+struct AppGroup: Decodable {
+    let feed: Feed
+}
+
+struct Feed: Decodable {
+    let title: String
+    let results: [FeedResult]
+}
+
+struct FeedResult: Decodable {
+    let name, artistName, artworkUrl100: String
+}
