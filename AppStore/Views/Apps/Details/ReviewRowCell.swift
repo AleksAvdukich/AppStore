9a2eefd0e8f4fcecda227b7ca7c314ef3cf0bcb0
Sanel Avdukich <avdukichsanel@mail.ru>
2019-10-17 19:05:35 +0300

    App Reviews Controller

---
+++ AppStore/Views/Apps/Details/ReviewRowCell.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewRowCell.swift
+//  AppStore
+//
+//  Created by Aleksandr Avdukich on 17/10/2019.
+//  Copyright © 2019 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class ReviewRowCell: UICollectionViewCell {
+    
+    let reviewsController = ReviewsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        addSubview(reviewsController.view)
+        reviewsController.view.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
