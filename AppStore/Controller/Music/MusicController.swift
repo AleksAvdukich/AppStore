0b1cd7e8e06b7846801574b1797116cc726c79a0
Sanel Avdukich <avdukichsanel@mail.ru>
2019-11-15 19:06:03 +0300

    Pagination Data Fetch

--- AppStore/Controller/Music/MusicController.swift
+++ AppStore/Controller/Music/MusicController.swift
@@ -19,6 +19,27 @@ class MusicController: BaseListController, UICollectionViewDelegateFlowLayout {
         
         collectionView.register(TrackCell.self, forCellWithReuseIdentifier: cellId)
         collectionView.register(MusicLoadingFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
+        
+        fetchData()
+    }
+    
+    var results = [Result]()
+    
+    fileprivate let searchTerm = "taylor"
+    
+    fileprivate func fetchData() {
+        let urlString = "https://itunes.apple.com/search?term=\(searchTerm)&offset=0&limit=20"
+        Service.shared.fetchGenericJSONData(urlString: urlString) { (searchResult: SearchResult?, err) in
+            
+            if let err = err {
+                print("Failed to paginate data:", err)
+            }
+            
+            self.results = searchResult?.results ?? []
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
     }
     
     //создание, настройка и возврат представления
@@ -28,15 +49,53 @@ class MusicController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return .init(width: view.frame.width, height: 100)
+        let height: CGFloat = isDonePaginating ? 0 : 100
+        return .init(width: view.frame.width, height: height)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return results.count
     }
+    
+    var isPaginating = false
+    var isDonePaginating = false
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TrackCell
+        
+        let track = results[indexPath.item]
+        
+        cell.nameLabel.text = track.trackName
+        cell.imageView.sd_setImage(with: URL(string: track.artworkUrl100))
+        cell.subtitleLabel.text = "\(track.artistName ?? "") • \(track.collectionName ?? "")"
+        
+        if indexPath.item == results.count - 1 && !isPaginating {
+            print("Fetch more data")
+            
+            isPaginating = true
+            
+            let urlString = "https://itunes.apple.com/search?term=\(searchTerm)&offset=\(results.count)&limit=20"
+            Service.shared.fetchGenericJSONData(urlString: urlString) { (searchResult: SearchResult?, err) in
+                
+                if let err = err {
+                    print("Failed to paginate data:", err)
+                }
+                
+                if searchResult?.results.count == 0 {
+                    self.isDonePaginating = true
+                }
+                
+                sleep(2)
+                
+                self.results = searchResult?.results ?? []
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+                self.isPaginating = false
+            }
+
+        }
+        
         return cell
     }
     
